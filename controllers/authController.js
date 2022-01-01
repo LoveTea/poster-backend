78cf5140ff800413ebba538be3097005aace84e6
Alex <lovetea@mail.ru>
2022-01-01 04:46:32 +0300

    added auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,45 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const signToken = (id, email) => {
+    return jwt.sign({ id, email }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new AppError("Введите email и пароль", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError("Некорректный Email или пароль", 401));
+    }
+
+    const token = signToken(user.id, user.email);
+
+    res.status(200).json({
+        status: "success",
+        token,
+    });
+});
+
+exports.signup = catchAsync(async (req, res) => {
+    const newUser = await User.create({
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    const token = signToken(newUser.id, newUser.email);
+
+    res.status(201).json({
+        status: "success",
+        token,
+    });
+});
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
 
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("hello")
-})
+mongoose
+    .connect(
+        `mongodb+srv://tea:${process.env.DB_PASSWORD}@postercluster.krjar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log("DB CONNECT SUCCESS");
+    })
+    .catch(() => {
+        console.log("DB CONNECT ERROR");
+    });
 
-const PORT = process.env.PORT
+app.use("/api/v1/users", userRoutes);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`server started on ${PORT} port`)
-})
+    console.log(`server started on ${PORT} port`);
+});
